5978e9da54abdef71be3073b79d2b68667f15232
Dominik Ciesielski <123950385+domcie99@users.noreply.github.com>
2023-12-17 19:55:53 +0100

    Add files via upload

---
+++ config.h
@@ -0,0 +1,11 @@
+// config.h
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+const char *SSID = "Dominik_Swiatlowod";
+const char *PASSWORD = "883102999";
+const char *THINGSPEAK_API = "P01G23AO1NLAK2IP";
+const unsigned long CHANNEL_ID = 2374607;
+
+#endif // CONFIG_H
